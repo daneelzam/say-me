2064d1bf10eec660cd274897a6539db54c7b54e8
Valeria <valeria.vol@gmail.com>
2021-02-04 13:27:05 +0300

    release 0

---
+++ client/src/components/Account/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login(props) {
+    return (
+        <div>
+            <form>
+              <label></label>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
