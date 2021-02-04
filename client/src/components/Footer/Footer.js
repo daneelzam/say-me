ab5772af6e7afa65283984de03f3a58c9b0c9e1a
Viktorya.Ledneva <viktorya.ledneva@yandex.ru>
2021-02-04 14:45:48 +0300

    header navbar logo footer account

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+      <section id="footer">
+      <div id="copyright">
+          <ul>
+              <li>&copy; SAY ME</li><li>Design: <a href="#">VLDO</a></li>
+          </ul>
+      </div>
+      </section>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Header/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Logo() {
+  return (
+    <div id="logo">
+     <h1>Say me</h1>
+     <p>Tell your partner what's going on with you.</p>
+    </div>
+  );
+}
+
+export default Logo;
