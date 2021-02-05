143806c38bd1266cf7cec32b6b00ce2ef87767fd
Viktorya.Ledneva <viktorya.ledneva@yandex.ru>
2021-02-05 15:33:12 +0300

    style module login

--- client/src/components/Account/Login/Login.js
+++ client/src/components/Account/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import './Login.css';
+import style from './Login.module.css';
 
 function Login() {
   const [name, setName] = useState('');
@@ -56,35 +56,39 @@ function Login() {
   };
 
   return (
-        <div className="container" ref={containerRef} id="container">
-            <div className="form-container sign-up-container">
-                <form onSubmit={signUpHandler}>
-                    <input type="text" placeholder="Name" onChange={handleName}/>
-                    <input type="text" placeholder="Email" onChange={handleEmail}/>
-                    <input type="password" placeholder="Password" onChange={handlePassword}/>
-                    <button>Sign Up</button>
+        <div className={style.container} ref={containerRef} id="container">
+            <div className={`${style.form_container} ${style.sign_up_container}`}>
+                <form className={style.form} onSubmit={signUpHandler}>
+                    <input className={style.input} type="text" placeholder="Name" onChange={handleName}/>
+                    <input className={style.input} type="text" placeholder="Email" onChange={handleEmail}/>
+                    <input className={style.input} type="password" placeholder="Password" onChange={handlePassword}/>
+                    <button className={style.button}>Sign Up</button>
                 </form>
             </div>
-            <div className="form-container sign-in-container">
-                <form onSubmit={loginHandler} >
-                    <h1>Sign in</h1>
-                    <input type="text" placeholder="Email" onChange={handleEmail}/>
-                    <input type="password" placeholder="Password" onChange={handlePassword} />
-                    <a href="#">Forgot your password?</a>
-                    <button>Sign In</button>
+            <div className={`${style.form_container} ${style.sign_in_container}`}>
+                <form className={style.form} onSubmit={loginHandler} >
+                    <h1 className={style.h}>Sign in</h1>
+                    <input className={style.input} type="text" placeholder="Email" onChange={handleEmail}/>
+                    <input className={style.input} type="password" placeholder="Password" onChange={handlePassword} />
+                    <a className={style.a} href="#">Forgot your password?</a>
+                    <button className={style.button}>Sign In</button>
                 </form>
             </div>
-            <div className="overlay-container">
-                <div className="overlay">
-                    <div className="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button className="ghost" onClick={styleHandler} id="signIn">Sign In</button>
+            <div className={style.overlay_container}>
+                <div className={style.overlay}>
+                    <div className={`${style.overlay_panel} ${style.overlay_left}`}>
+                        <h1 className={style.h1}>Welcome Back!</h1>
+                        <p className={style.overlay_panel_p}>
+                          To keep connected with us please login with your personal info
+                        </p>
+                        <button className={`${style.ghost} ${style.button}`} onClick={styleHandler} id="signIn">Sign In</button>
                     </div>
-                    <div className="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button className="ghost" onClick={styleHandler} id="signUp">Sign Up</button>
+                    <div className={`${style.overlay_panel} ${style.overlay_right}`}>
+                        <h1 className={style.h1}>Hello, Friend!</h1>
+                        <p className={style.overlay_panel_p}>
+                          Enter your personal details and start journey with us
+                        </p>
+                        <button className={`${style.button} ${style.button_ghost}`} onClick={styleHandler} id="signUp">Sign Up</button>
                     </div>
                 </div>
             </div>
