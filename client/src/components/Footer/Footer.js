b48a8c163ba0779bb83e23a6aa4143dfb41b9118
Daniel Zameshaev <daneelzam@yandex.ru>
2021-02-05 11:55:28 +0300

    footer module style

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './Footer.css';
+import style from './Footer.module.css';
 
 function Footer() {
   return (
-      <section id="footer">
-      <div id="copyright">
-          <ul>
-              <li>&copy; SAY ME</li><li>Design: <a href="#">VLDO</a></li>
+      <section className={style.footer}>
+      <div className={style.copyright}>
+          <ul className={style.copyright_ul}>
+              <li className={style.copyright_ul_li}>&copy; SAY ME</li>
+              <li className={style.copyright_ul_li}>Design: <a className={style.copyright_a} href="#">VLDO</a></li>
           </ul>
       </div>
       </section>
