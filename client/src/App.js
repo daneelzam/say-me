4add6e0fe181cdf2621924be1c6f54818dd0c1e0
Daniel Zameshaev <daneelzam@yandex.ru>
2021-02-05 13:10:24 +0300

    login style

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Switch, Route, Link
+  BrowserRouter as Router, Switch, Route
 } from 'react-router-dom';
 import Login from './components/Account/Login/Login';
 import Footer from './components/Footer/Footer';
@@ -11,39 +11,20 @@ import Account from './components/Account/Account/Account';
 import Calendar from './components/Calendar/Calendar';
 
 function App() {
-  const isAuth = true;
+  const isAuth = false;
   const Logout = () => <div children="logout"/>;
-  const renderHeader = () => {
-    if (isAuth) {
-      return <Header/>;
-    }
-    return (
-        <nav>
-          <ul>
-            {/* <li> */}
-            {/*  <Link to="/">Home</Link> */}
-            {/* </li> */}
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            {/* <li>
-              <Link to="/signup">SignUp</Link>
-            </li> */}
-          </ul>
-        </nav>
-    );
-  };
+
   return (
       <Router>
         <div>
-          {renderHeader()}
-          {isAuth && <Footer/>}
+          {isAuth && <Header/>}
           <Switch>
-              <Route path="/login"><Login/></Route>
+            <Route path="/login"><Login/></Route>
             <ProtectedRouter Component={Account} path="/account"/>
             <ProtectedRouter Component={Calendar} path="/" />
             <ProtectedRouter Component={Logout} path="auth/logout"/>
           </Switch>
+          {isAuth && <Footer/>}
         </div>
       </Router>
   );
