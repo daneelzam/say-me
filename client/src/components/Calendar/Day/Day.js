7adc1d285fa795d868ac6ae271a5ed0de48e7239
Valeria <valeria.vol@gmail.com>
2021-02-09 12:34:22 +0300

    period

--- client/src/components/Calendar/Day/Day.js
+++ client/src/components/Calendar/Day/Day.js
@@ -4,31 +4,35 @@ import style from './Day.module.css';
 import { chooseDayAC } from '../../../redux/actionCreators/calendarAC';
 
 function Day({ year, month, date }) {
-  // const periodDays = useSelector((state) => state.calendar.periodStart);
-  // const chooseDay = useSelector((state) => state.calendar.chooseDay);
+  const periodDays = useSelector((state) => state.calendar.periodStart);
+  const chooseDay = useSelector((state) => state.calendar.chooseDay);
   const dispatch = useDispatch();
-  const chooseday = useSelector((state) => state.calendar.chooseDay);
   const [options] = useState({
     weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
   });
+
   const [currentDate] = useState(new Date(year, month, date));
   const dayRef = useRef();
+
   useEffect(() => {
-    if (currentDate.toLocaleDateString('en-US', options) === chooseday) {
+    if (currentDate.toLocaleDateString('en-US', options) === chooseDay) {
       dayRef.current.classList.add(`${style.days_td_selected}`);
     } else {
       dayRef.current.classList.remove(`${style.days_td_selected}`);
     }
-  }, [chooseday]);
+  }, [chooseDay]);
+
+  useEffect(() => {
+    if (periodDays.includes(currentDate.toLocaleDateString('en-US', options))) {
+      dayRef.current.classList.add(`${style.day_td_period}`);
+    } else {
+      dayRef.current.classList.remove(`${style.day_td_period}`);
+    }
+  });
+
   const setActive = () => {
     dispatch(chooseDayAC(currentDate.toLocaleDateString('en-US', options)));
   };
-
-  // useEffect(() => {
-  //   if (periodDays.includes(chooseDay)) {
-  //
-  //   }
-  // }, [periodDays]);
 
   return (
         <>
